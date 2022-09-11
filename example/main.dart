4e58aedbbc0058f42af7583ddc86d0588ce3f774
Armiron Kurbalaj <armironkurbalaj@gmail.com>
2022-09-11 16:09:37 +0200

    Added finishing touches to prompter and teminal  classes

--- example/main.dart
+++ example/main.dart
@@ -1,10 +1,12 @@
-import 'package:prompter_ak/src/terminal.dart';
 import 'package:prompter_ak/src/option.dart';
+import 'package:prompter_ak/src/prompter.dart';
 
 void main(List<String> args) {
-  var terminal = Terminal();
   var options = [Option("I want red", '#f00'), Option("I want blue", '#00f')];
-  terminal.printOptions(options);
-  var response = terminal.collectInput();
-  print(response);
+  var prompter = Prompter();
+  final userPick = prompter.askBinary("Do you like dart");
+  print(userPick);
+
+  final userPick2 = prompter.askMultiple("What color do you want", options);
+  print(userPick2);
 }
